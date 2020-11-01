394d886d8841151fdad8a2cdc42fa77fe3646ae3
Wendril Adam <adamwendril@gmail.com>
2020-11-01 11:35:11 -0300

    Criados Modelos de Livro e Cliente.

---
+++ WebApp/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.WebApp.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string CPF { get; set; }
+        public string NomeCompleto { get; set; }
+        public DateTime DataNasc { get; set; }
+    }
+}
---
+++ WebApp/Models/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.WebApp.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Título { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+    }
+}
